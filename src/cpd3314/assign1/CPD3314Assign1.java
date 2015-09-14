4aab3574fe66ec31d6d1dc9259eef3611988d009
Toshif <C0657050@mylambton.ca>
2015-09-14 13:54:46 -0400

    Hello

--- src/cpd3314/assign1/CPD3314Assign1.java
+++ src/cpd3314/assign1/CPD3314Assign1.java
@@ -16,7 +16,7 @@ public class CPD3314Assign1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ok");
+        System.out.println("Hello World");
     }
     
 }
