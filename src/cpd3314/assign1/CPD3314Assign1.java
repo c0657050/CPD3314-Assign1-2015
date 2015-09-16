f672f2050860f9aebaabb5a92f02a7e79f597ed3
c0657050 <C0657050@mylambton.ca>
2015-09-15 20:09:23 -0400

    Update CPD3314Assign1.java

--- src/cpd3314/assign1/CPD3314Assign1.java
+++ src/cpd3314/assign1/CPD3314Assign1.java
@@ -16,7 +16,24 @@ public class CPD3314Assign1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /*********
+         * ASSIGNMENT 1
+         * Job Posting
+         * JOB TITLE - Intermediate Java Developer
+         * LOCATION- Montreal, QC
+         * SKILLS REQUIRE -
+         * Java      - https://en.wikipedia.org/wiki/Java_(programming_language)
+         * JSP       - https://en.wikipedia.org/wiki/JavaServer_Pages
+         * J2EE      - https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+         * Struts2   - https://en.wikipedia.org/wiki/Apache_Struts_2
+         * Spring    - https://en.wikipedia.org/wiki/Apache_Struts_2
+         * Hibernate - https://en.wikipedia.org/wiki/Apache_Struts_2
+         * 
+         * Potential Salary
+         * Toronto   - 72K
+         * Vancouver - 76K
+         * Waterloo  - 66K
+         * */
         System.out.println("Hello World");
     }
-    
 }
